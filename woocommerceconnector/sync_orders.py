ae42fc1b0e90e4ac3fc57db870e10e8b441acbc0
Vit Selvarajah <vithu@live.com>
2023-04-11 21:54:09 +0100

    Enable Taxes

--- woocommerceconnector/sync_orders.py
+++ woocommerceconnector/sync_orders.py
@@ -245,7 +245,7 @@ def create_sales_order(woocommerce_order, woocommerce_settings, company=None):
             "selling_price_list": woocommerce_settings.price_list,
             "ignore_pricing_rule": 1,
             "items": get_order_items(woocommerce_order.get("line_items"), woocommerce_settings),
-            # "taxes": get_order_taxes(woocommerce_order, woocommerce_settings),
+            "taxes": get_order_taxes(woocommerce_order, woocommerce_settings),
             # disabled discount as WooCommerce will send this both in the item rate and as discount
             #"apply_discount_on": "Net Total",
             #"discount_amount": flt(woocommerce_order.get("discount_total") or 0),
